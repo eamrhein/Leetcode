f0736e285093df5054cd1842869596408365945a
Eric Amrhein <eamrhein@protonmail.ch>
2019-11-22 17:06:23 -0800

    Update moveZeros.js

--- moveZeros.js
+++ moveZeros.js
@@ -1,13 +1,10 @@
 var moveZeroes = function(nums) {
     let zeroCount = 0;
-    nums.forEach((num) => {
-        if (num === 0) {
-            zeroCount += 1;
-        }
-    })
     for(let i = 0; i < nums.length; i++) {
-      if(num === 0) {
+      if(nums[i] === 0) {
+        zeroCount += 1;
         nums.splice(i, 1);
+        i-=1;
       }
     }
     let zeros = new Array(zeroCount).fill(0)
